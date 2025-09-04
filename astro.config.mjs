8ee53c37bbb425e36990cdee985da0fa0c37c980
rileysklar <riley.sklar@gmail.com>
2025-09-04 15:28:47 -0500

    Convert to static output for Netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import node from "@astrojs/node";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
-  server: {
-    port: 4333
-  },
+  output: 'static',
   integrations: [react(), tailwind()],
 });
